235fdf348051c8f348d9dacd11c6a2aa18721751
pc_home <rosa.henrique@outlook.com.br>
2020-07-09 01:24:57 -0300

    added base to user

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UserModule } from '../user/user.module';
 import * as ormconfig from './config/orm.config';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(ormconfig)],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TypeOrmModule.forRoot(ormconfig), UserModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
