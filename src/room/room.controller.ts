1b437c90dd96bcf5b3b4dba163b9408225b084e2
Henrique Rosa <rosa.henrique@outlook.com.br>
2020-07-19 20:24:30 -0300

    inital commit for room

--- src/room/room.controller.ts
+++ src/room/room.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, SetMetadata, UseGuards } from '@nestjs/common';
 import { RoomService } from './room.service';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { RolesGuard } from 'src/auth/roles.guard';
+import { RoomDTO } from './dto/room.dto';
 
 @Controller('room')
 export class RoomController {
@@ -10,7 +11,7 @@ export class RoomController {
   @SetMetadata('roles', ['admin'])
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Get()
-  async getAll(): Promise<any> {
+  async getAll(): Promise<RoomDTO[]> {
     return this.service.getAll();
   }
 }
--- src/room/room.service.ts
+++ src/room/room.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { RoomEntity } from './room.entity';
 import { Repository } from 'typeorm';
+import { RoomDTO } from './dto/room.dto';
+import { plainToClass } from 'class-transformer';
 
 @Injectable()
 export class RoomService {
@@ -10,9 +12,8 @@ export class RoomService {
     private readonly rep: Repository<RoomEntity>,
   ) {}
 
-  async getAll(): Promise<any> {
-    //const building = await this.rep.find();
-    //return plainToClass(BuildingDTO, building);
-    return await this.rep.find();
+  async getAll(): Promise<RoomDTO[]> {
+    const rooms = await this.rep.find();
+    return plainToClass(RoomDTO, rooms);
   }
 }
